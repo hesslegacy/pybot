16cc6523dde12df5496351c3012124ddb5bd1d93
hesslegacy <117324941+hesslegacy@users.noreply.github.com>
2023-02-27 19:45:33 +0000

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -2,9 +2,11 @@
 
 echo "lets get things set up!..."
 sleep 2
-pip install discord.py
+python3 -m pip install --upgrade pip
 sleep 2
-pip install ffmpeg
+python3 -m pip install discord
 sleep 2
-pip install youtube_dl
+python3 -m pip install ffmpeg
+sleep 2
+python3 -m pip install youtube_dl
 sleep 2
